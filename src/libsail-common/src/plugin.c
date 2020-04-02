94531686149bded7ad0422a999f9dd9708932616
Dmitry Baryshev <dmitrymq@gmail.com>
2020-04-02 23:46:31 +0300

    COMMON: Implement plugin info loader

---
+++ src/libsail-common/src/plugin.c
@@ -0,0 +1,114 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "plugin.h"
+#include "ini.h"
+#include "utils.h"
+
+int sail_plugin_info_alloc(struct sail_plugin_info **plugin_info) {
+
+    *plugin_info = (struct sail_plugin_info *)malloc(sizeof(struct sail_plugin_info));
+
+    if (*plugin_info == NULL) {
+        return ENOMEM;
+    }
+
+    (*plugin_info)->layout      = 0;
+    (*plugin_info)->version     = NULL;
+    (*plugin_info)->description = NULL;
+    (*plugin_info)->extensions  = NULL;
+    (*plugin_info)->mime_types  = NULL;
+    (*plugin_info)->magic       = NULL;
+
+    return 0;
+}
+
+void sail_plugin_info_destroy(struct sail_plugin_info *plugin_info) {
+
+    if (plugin_info == NULL) {
+        return;
+    }
+
+    if (plugin_info->version != NULL) {
+        free(plugin_info->version);
+    }
+    if (plugin_info->description != NULL) {
+        free(plugin_info->description);
+    }
+    if (plugin_info->extensions != NULL) {
+        free(plugin_info->extensions);
+    }
+    if (plugin_info->mime_types != NULL) {
+        free(plugin_info->mime_types);
+    }
+    if (plugin_info->magic != NULL) {
+        free(plugin_info->magic);
+    }
+
+    free(plugin_info);
+}
+
+static int inih_handler(void *data, const char *section, const char *name, const char *value) {
+
+    (void)section;
+
+    struct sail_plugin_info *plugin_info = (struct sail_plugin_info *)data;
+
+    int res;
+
+    if (strcmp(name, "layout") == 0) {
+        plugin_info->layout = atoi(value);
+    } else if (strcmp(name, "version") == 0) {
+        if ((res = sail_strdup(value, &plugin_info->version)) != 0) {
+            return 0;
+        }
+    } else if (strcmp(name, "description") == 0) {
+        if ((res = sail_strdup(value, &plugin_info->description)) != 0) {
+            return 0;
+        }
+    } else if (strcmp(name, "extensions") == 0) {
+        if ((res = sail_strdup(value, &plugin_info->extensions)) != 0) {
+            return 0;
+        }
+    } else if (strcmp(name, "mime-types") == 0) {
+        if ((res = sail_strdup(value, &plugin_info->mime_types)) != 0) {
+            return 0;
+        }
+    } else if (strcmp(name, "magic") == 0) {
+        if ((res = sail_strdup(value, &plugin_info->magic)) != 0) {
+            return 0;
+        }
+    } else {
+        fprintf(stderr, "SAIL: Unsupported plugin configuraton key '%s'\n", name);
+        return 0;  /* error */
+    }
+
+    return 1;
+}
+
+int sail_plugin_read_info(const char *file, struct sail_plugin_info **plugin_info) {
+
+    if (file == NULL) {
+        return EINVAL;
+    }
+
+    int res;
+
+    if ((res = sail_plugin_info_alloc(plugin_info)) != 0) {
+        return res;
+    }
+
+    /*
+     * Returns 0 on success, line number of first error on parse error (doesn't
+     * stop on first error), -1 on file open error, or -2 on memory allocation
+     * error (only when INI_USE_STACK is zero).
+     */
+    if (ini_parse(file, inih_handler, *plugin_info) != 0) {
+        sail_plugin_info_destroy(*plugin_info);
+        return EIO;
+    }
+
+    return 0;
+}
---
+++ src/libsail-common/src/plugin.h
@@ -0,0 +1,82 @@
+#ifndef SAIL_PLUGIN_H
+#define SAIL_PLUGIN_H
+
+#ifdef SAIL_BUILD
+    #include "export.h"
+#else
+    #include <sail/export.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * A structure representing an image.
+ */
+struct sail_plugin_info {
+
+    /*
+     * The plugin loader will use the plugin's layout version to correctly handle the plugin. Unsupported
+     * plugin layout versions will be reported.
+     *
+     * Plugin layout is a list of exported functions. We use plugin layout versions to implement
+     * backward compatibility in a simple and maintanable way.
+     */
+    int layout;
+
+    /* Plugin version. For example: "1.5.2". */
+    char *version;
+
+    /* Plugin description. For example: "JPEG image". */
+    char *description;
+
+    /* Semicolon-separated list of supported file extensions. For example: "jpg;jpeg". */
+    char *extensions;
+
+    /* Semicolon-separated list of supported file mime types. For example: "image/jpeg". */
+    char *mime_types;
+
+    /*
+     * Magic identifier as a regex string. For example: "GIF8[79]a".
+     *
+     * See https://en.wikipedia.org/wiki/List_of_file_signatures for more.
+     */
+    char *magic;
+};
+
+typedef struct sail_plugin_info sail_plugin_info_t;
+
+/*
+ * Plugin info functions.
+ */
+
+/*
+ * Allocates a new plugin info object. The assigned plugin info MUST be destroyed later
+ * with sail_plugin_info_destroy().
+ *
+ * Returns 0 on success or errno on error.
+ */
+int SAIL_EXPORT sail_plugin_info_alloc(struct sail_plugin_info **plugin_info);
+
+/*
+ * Destroys the specified plugin_info and all its internal allocated memory buffers.
+ * The "plugin_info" pointer MUST NOT be used after calling this function.
+ */
+void SAIL_EXPORT sail_plugin_info_destroy(struct sail_plugin_info *plugin_info);
+
+/*
+ * Reads SAIL plugin info from the specified file and stores the parsed information into
+ * the specified plugin info object. The assigned plugin info MUST be destroyed later
+ * with sail_plugin_info_destroy().
+ *
+ * Returns 0 on success or errno on error.
+ */
+int SAIL_EXPORT sail_plugin_read_info(const char *file, struct sail_plugin_info **plugin_info);
+
+/* extern "C" */
+#ifdef __cplusplus
+}
+#endif
+
+#endif
