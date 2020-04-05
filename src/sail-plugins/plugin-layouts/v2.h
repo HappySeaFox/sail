5368288ef43b0e6bfede48771b4c0bbcf3418a05
Dmitry Baryshev <dmitrymq@gmail.com>
2020-04-05 18:04:38 +0300

    PLUGINS: Added v2 layout definition file

---
+++ src/sail-plugins/plugin-layouts/v2.h
@@ -0,0 +1,29 @@
+/*
+ * This is a plugin layout definition file (version 2) created just for
+ * informational purposes. It's not used in SAIL and not to be included
+ * anywhere. Use this file just for reference how plugins version 2
+ * are organized.
+ */
+
+/*
+ * Generate a syntax error.
+ */
+NOT TO BE INCLUDED
+
+/* V2 inherits all the functions from V1. */
+
+/*
+ * Decoding functions.
+ */
+
+/*
+ * Reads a scan line of the current image in the current pass. Allocates a new scan line. The assigned scan line
+ * MUST be freed later with free().
+ *
+ * Returns 0 on success or errno on error.
+ */
+int sail_plugin_read_scan_line_v1(struct sail_file *file, struct sail_image *image, void **scanline);
+
+/*
+ * Encoding functions.
+ */
