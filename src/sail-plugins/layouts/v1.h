9a9fae295148f9e6a4fe5dc0a5d60f7dc5388fe6
Dmitry Baryshev <dmitrymq@gmail.com>
2020-04-08 16:18:55 +0300

    PLUGINS: Install plugins layouts to simplify plugins' debugging

--- src/sail-plugins/layouts/v1.h
+++ src/sail-plugins/layouts/v1.h
@@ -1,14 +1,20 @@
-/*
- * This is a plugin layout definition file (version 1) created just for
- * informational purposes. It's not used in SAIL and not to be included
- * anywhere. Use this file just for reference how plugins version 1
- * are organized.
- */
+#ifndef SAIL_PLUGIN_LAYOUT_V1_H
+#define SAIL_PLUGIN_LAYOUT_V1_H
 
 /*
- * Generate a syntax error.
+ * This is a plugin layout definition file (version 1).
+ *
+ * It's intedened to be used as a reference how plugins V1 are organized. It's also could
+ * be used by plugins' developers to compile their plugins directly into a testing application
+ * to simplify debugging.
  */
-NOT TO BE INCLUDED
+
+#include <sail/error.h>
+#include <sail/export.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /*
  * Decoding functions.
@@ -20,7 +26,7 @@ NOT TO BE INCLUDED
  *
  * Returns 0 on success or sail_error_t on error.
  */
-sail_error_t sail_plugin_read_features_v1(struct sail_read_features **read_features);
+SAIL_EXPORT sail_error_t sail_plugin_read_features_v1(struct sail_read_features **read_features);
 
 /*
  * Starts decoding the specified file using the specified options (or NULL to use defaults).
@@ -30,7 +36,7 @@ sail_error_t sail_plugin_read_features_v1(struct sail_read_features **read_featu
  *
  * Returns 0 on success or sail_error_t on error.
  */
-sail_error_t sail_plugin_read_init_v1(struct sail_file *file, struct sail_read_options *read_options);
+SAIL_EXPORT sail_error_t sail_plugin_read_init_v1(struct sail_file *file, struct sail_read_options *read_options);
 
 /*
  * Seeks to the next frame. The frame is NOT immediately read or decoded by most SAIL plugins. One could
@@ -41,14 +47,14 @@ sail_error_t sail_plugin_read_init_v1(struct sail_file *file, struct sail_read_o
  *
  * Returns 0 on success or sail_error_t on error.
  */
-sail_error_t sail_plugin_read_seek_next_frame_v1(struct sail_file *file, struct sail_image **image);
+SAIL_EXPORT sail_error_t sail_plugin_read_seek_next_frame_v1(struct sail_file *file, struct sail_image **image);
 
 /*
  * Seeks to the next pass if the specified image has multiple passes. Does nothing otherwise.
  *
  * Returns 0 on success or sail_error_t on error.
  */
-sail_error_t sail_plugin_read_seek_next_pass_v1(struct sail_file *file, struct sail_image *image);
+SAIL_EXPORT sail_error_t sail_plugin_read_seek_next_pass_v1(struct sail_file *file, struct sail_image *image);
 
 /*
  * Reads a scan line of the current image in the current pass. The specified scan line must be
@@ -57,7 +63,7 @@ sail_error_t sail_plugin_read_seek_next_pass_v1(struct sail_file *file, struct s
  *
  * Returns 0 on success or sail_error_t on error.
  */
-sail_error_t sail_plugin_read_scan_line_v1(struct sail_file *file, struct sail_image *image, void *scanline);
+SAIL_EXPORT sail_error_t sail_plugin_read_scan_line_v1(struct sail_file *file, struct sail_image *image, void *scanline);
 
 /*
  * Finilizes reading operation. No more readings are possible after calling this function.
@@ -66,7 +72,7 @@ sail_error_t sail_plugin_read_scan_line_v1(struct sail_file *file, struct sail_i
  *
  * Returns 0 on success or sail_error_t on error.
  */
-sail_error_t sail_plugin_read_finish_v1(struct sail_file *file, struct sail_image *image);
+SAIL_EXPORT sail_error_t sail_plugin_read_finish_v1(struct sail_file *file, struct sail_image *image);
 
 /*
  * Encoding functions.
@@ -78,7 +84,7 @@ sail_error_t sail_plugin_read_finish_v1(struct sail_file *file, struct sail_imag
  *
  * Returns 0 on success or sail_error_t on error.
  */
-sail_error_t sail_plugin_write_features_v1(struct sail_write_features **write_features);
+SAIL_EXPORT sail_error_t sail_plugin_write_features_v1(struct sail_write_features **write_features);
 
 /*
  * Starts encoding the specified file using the specified options (or NULL to use defaults).
@@ -88,7 +94,7 @@ sail_error_t sail_plugin_write_features_v1(struct sail_write_features **write_fe
  *
  * Returns 0 on success or sail_error_t on error.
  */
-sail_error_t sail_plugin_write_init_v1(struct sail_file *file, struct sail_write_options *write_options);
+SAIL_EXPORT sail_error_t sail_plugin_write_init_v1(struct sail_file *file, struct sail_write_options *write_options);
 
 /*
  * Seeks to a next frame before writing it. The frame is NOT immediately written. Use sail_plugin_write_seek_next_pass()
@@ -96,21 +102,21 @@ sail_error_t sail_plugin_write_init_v1(struct sail_file *file, struct sail_write
  *
  * Returns 0 on success or sail_error_t on error.
  */
-sail_error_t sail_plugin_write_seek_next_frame_v1(struct sail_file *file, struct sail_image *image);
+SAIL_EXPORT sail_error_t sail_plugin_write_seek_next_frame_v1(struct sail_file *file, struct sail_image *image);
 
 /*
  * Seeks to a next pass before writing it if the specified image is interlaced. Does nothing otherwise.
  *
  * Returns 0 on success or sail_error_t on error.
  */
-sail_error_t sail_plugin_write_seek_next_pass_v1(struct sail_file *file, struct sail_image *image);
+SAIL_EXPORT sail_error_t sail_plugin_write_seek_next_pass_v1(struct sail_file *file, struct sail_image *image);
 
 /*
  * Writes a scan line of the current image in the current pass.
  *
  * Returns 0 on success or sail_error_t on error.
  */
-sail_error_t sail_plugin_write_scan_line_v1(struct sail_file *file, struct sail_image *image, void *scanline);
+SAIL_EXPORT sail_error_t sail_plugin_write_scan_line_v1(struct sail_file *file, struct sail_image *image, void *scanline);
 
 /*
  * Finilizes writing operation. No more writings are possible after calling this function.
@@ -118,4 +124,11 @@ sail_error_t sail_plugin_write_scan_line_v1(struct sail_file *file, struct sail_
  *
  * Returns 0 on success or sail_error_t on error.
  */
-sail_error_t sail_plugin_write_finish_v1(struct sail_file *file, struct sail_image *image);
+SAIL_EXPORT sail_error_t sail_plugin_write_finish_v1(struct sail_file *file, struct sail_image *image);
+
+/* extern "C" */
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/sail-plugins/layouts/v2.h
@@ -0,0 +1,44 @@
+#ifndef SAIL_PLUGIN_LAYOUT_V2_H
+#define SAIL_PLUGIN_LAYOUT_V2_H
+
+/*
+ * This is a plugin layout definition file (version 2).
+ *
+ * It's intedened to be used as a reference how plugins V2 are organized. It's also could
+ * be used by plugins' developers to compile their plugins directly into a testing application
+ * to simplify debugging.
+ *
+ * V2 inherits all the functions from V1.
+ */
+
+#include <sail/error.h>
+#include <sail/export.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* V2 inherits all the functions from V1. */
+
+/*
+ * Decoding functions.
+ */
+
+/*
+ * Reads a scan line of the current image in the current pass. Allocates a new scan line. The assigned scan line
+ * MUST be freed later with free().
+ *
+ * Returns 0 on success or sail_error_t on error.
+ */
+SAIL_EXPORT sail_error_t sail_plugin_read_scan_line_v2(struct sail_file *file, struct sail_image *image, void **scanline);
+
+/*
+ * Encoding functions.
+ */
+
+/* extern "C" */
+#ifdef __cplusplus
+}
+#endif
+
+#endif
