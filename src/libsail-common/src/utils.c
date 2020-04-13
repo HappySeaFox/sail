5d5eecd098ccf1251daa6d2cf17a783c493b42c2
Dmitry Baryshev <dmitrymq@gmail.com>
2020-04-13 09:37:08 +0300

    SAIL: Added license headers

--- src/libsail-common/src/utils.c
+++ src/libsail-common/src/utils.c
@@ -1,3 +1,21 @@
+/*  This file is part of SAIL (https://github.com/sailor-keg/sail)
+
+    Copyright (c) 2020 Dmitry Baryshev <dmitrymq@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 3 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
