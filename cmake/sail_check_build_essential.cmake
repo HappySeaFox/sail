7638b88fa1278e30943cc8706cd067f2435e327f
Dmitry Baryshev <dmitrymq@gmail.com>
2020-05-14 17:51:12 +0300

    SAIL: Check for required includes

---
+++ cmake/sail_check_build_essential.cmake
@@ -0,0 +1,19 @@
+include(CheckIncludeFile)
+
+# Intended to be included by SAIL.
+#
+macro(sail_check_include)
+    set(SAIL_CHECK_INCLUDE "${ARGV0}")
+
+    # Build "HAVE_STDIO_H"
+    #
+    string(TOUPPER "${SAIL_CHECK_INCLUDE}" HAVE_THIS_H)
+    string(REGEX REPLACE "[/\\.]" "_" HAVE_THIS_H "${HAVE_THIS_H}")
+    set(HAVE_THIS_H "HAVE_${HAVE_THIS_H}")
+
+    check_include_file("${SAIL_CHECK_INCLUDE}" "${HAVE_THIS_H}")
+
+    if (NOT "${${HAVE_THIS_H}}")
+        message(FATAL_ERROR "${SAIL_CHECK_INCLUDE} include file is not found. Please check the required development packages are installed.")
+    endif()
+endmacro()
