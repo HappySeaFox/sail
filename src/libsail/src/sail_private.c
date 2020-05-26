c10f4125bbffcc21c7a221e99953bf3fae5fa232
Dmitry Baryshev <dmitrymq@gmail.com>
2020-05-26 15:53:53 +0300

    LIBSAIL: Fixed double-free

--- src/libsail/src/sail_private.c
+++ src/libsail/src/sail_private.c
@@ -93,8 +93,6 @@ void destroy_hidden_state(struct hidden_state *state) {
     /* This state must be freed and zeroed by plugins. We free it just in case to avoid memory leaks. */
     free(state->state);
 
-    free(state->write_options);
-
     free(state);
 }
 
