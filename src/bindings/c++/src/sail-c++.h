cba9c84b699bc66f2b3a889ed36ed1cac1d3e864
Dmitry Baryshev <dmitrymq@gmail.com>
2020-06-02 15:29:44 +0300

    BINDINGS/C++: Revert "BINDINGS/C++: Remove at_scope_exit-c++.h"
    This reverts commit 67ed9ab3cb103a6f0eef0899e8a603feab4985b1.

--- src/bindings/c++/src/sail-c++.h
+++ src/bindings/c++/src/sail-c++.h
@@ -24,6 +24,7 @@
 #ifdef SAIL_BUILD
     #include "sail-common.h"
 
+    #include "at_scope_exit-c++.h"
     #include "context-c++.h"
     #include "image-c++.h"
     #include "image_reader-c++.h"
@@ -38,6 +39,7 @@
 #else
     #include <sail-common/sail-common.h>
 
+    #include <sail-c++/at_scope_exit-c++.h>
     #include <sail-c++/context-c++.h>
     #include <sail-c++/image-c++.h>
     #include <sail-c++/image_reader-c++.h>
