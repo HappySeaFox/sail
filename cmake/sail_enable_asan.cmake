0eeb9b20b0eb930c8b60717bb05504e94ca29dfc
Dmitry Baryshev <dmitrymq@gmail.com>
2020-06-10 16:58:37 +0300

    SAIL: Rename SAIL_ASAN var to SAIL_ASAN_FLAGS

--- cmake/sail_enable_asan.cmake
+++ cmake/sail_enable_asan.cmake
@@ -3,14 +3,14 @@
 macro(sail_enable_asan)
     cmake_parse_arguments(SAIL_ASAN "" "TARGET" "" ${ARGN})
 
-    # Enable ASAN on the specified target if possible
+    # Enable ASAN on the specified target if possible. SAIL_ASAN_FLAGS is set in the main cmake file
     #
-    if (SAIL_ASAN)
+    if (SAIL_ASAN_FLAGS)
         if (CMAKE_COMPILER_IS_GNUCC)
-            target_compile_options("${SAIL_ASAN_TARGET}" PRIVATE "${SAIL_ASAN}")
-            target_link_libraries("${SAIL_ASAN_TARGET}" "${SAIL_ASAN}")
+            target_compile_options("${SAIL_ASAN_TARGET}" PRIVATE "${SAIL_ASAN_FLAGS}")
+            target_link_libraries("${SAIL_ASAN_TARGET}" "${SAIL_ASAN_FLAGS}")
         else()
-            message("ASAN is not supported on this platform.")
+            message("ASAN is not supported with this compiler.")
         endif()
     endif()
 endmacro()
