ee4d40e9dcb2c33b9341c0b0c1bc8b6754e87837
Dmitry Baryshev <dmitrymq@gmail.com>
2020-06-30 20:57:48 +0300

    EXAMPLES: Probe from memory in the C deep diver example

--- examples/c++/qt/deep-diver-with-c-api/qtsail.cpp
+++ examples/c++/qt/deep-diver-with-c-api/qtsail.cpp
@@ -418,7 +418,18 @@ sail_error_t QtSail::onProbe()
     const struct sail_plugin_info *plugin_info;
     sail_error_t res;
 
-    if ((res = sail_probe_file(path.toLocal8Bit(), m_context, &image, &plugin_info)) != 0) {
+    // Load the file into memeory
+    QFile file(path);
+
+    if (!file.open(QIODevice::ReadOnly)) {
+        QMessageBox::critical(this, tr("Error"), tr("Failed to open the file. Error: %1").arg(file.errorString()));
+        return SAIL_FILE_OPEN_ERROR;
+    }
+
+    const QByteArray buffer = file.readAll();
+
+    // Probe from memory
+    if ((res = sail_probe_mem(buffer.constData(), buffer.length(), m_context, &image, &plugin_info)) != 0) {
         QMessageBox::critical(this, tr("Error"), tr("Failed to probe the image. Error: %1").arg(res));
         return res;
     }
