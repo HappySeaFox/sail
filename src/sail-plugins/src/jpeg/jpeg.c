c690c1377b31fad7f35a30c3d551180bdc03e8cb
Dmitry Baryshev <dmitrymq@gmail.com>
2020-07-06 01:12:45 +0300

    JPEG: Fix setting bytes per line

--- src/sail-plugins/src/jpeg/jpeg.c
+++ src/sail-plugins/src/jpeg/jpeg.c
@@ -191,7 +191,7 @@ SAIL_EXPORT sail_error_t sail_plugin_read_seek_next_frame_v2(void *state, struct
 
     (*image)->width                      = jpeg_state->decompress_context.output_width;
     (*image)->height                     = jpeg_state->decompress_context.output_height;
-    (*image)->bytes_per_line             = jpeg_state->decompress_context.output_width * (JDIMENSION)jpeg_state->decompress_context.output_components;
+    (*image)->bytes_per_line             = bytes_per_line;
     (*image)->source_image->pixel_format = color_space_to_pixel_format(jpeg_state->decompress_context.jpeg_color_space);
 
     if (jpeg_state->read_options->output_pixel_format == SAIL_PIXEL_FORMAT_SOURCE) {
