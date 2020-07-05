2838d5551f5b2b6a145d6f67778dda9e4bee247d
Dmitry Baryshev <dmitrymq@gmail.com>
2020-07-05 22:19:54 +0300

    SAIL: Enabled ASAN with Clang

--- cmake/sail_enable_asan.cmake
+++ cmake/sail_enable_asan.cmake
@@ -3,10 +3,11 @@
 macro(sail_enable_asan)
     cmake_parse_arguments(SAIL_ASAN "" "TARGET" "" ${ARGN})
 
-    # Enable ASAN on the specified target in DEV mode only
+    # Enable ASAN on the specified target (with specific compilers) in DEV mode only
     #
     if (SAIL_DEV)
-        if (CMAKE_COMPILER_IS_GNUCC)
+        if (CMAKE_C_COMPILER_ID STREQUAL "GNU" OR CMAKE_C_COMPILER_ID MATCHES "Clang")
+            message("ASAN is enabled")
             target_compile_options("${SAIL_ASAN_TARGET}" PRIVATE "-fsanitize=address,leak")
             target_link_libraries("${SAIL_ASAN_TARGET}" "-fsanitize=address,leak")
         else()
