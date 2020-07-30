816565f35fbb86ed3d73a286af8e573cc18e4509
Dmitry Baryshev <dmitrymq@gmail.com>
2020-07-30 19:48:58 +0300

    TIFF: Fix using WEBP and ZSTD compressions with old TIFF verions

--- src/sail-plugins/src/tiff/helpers.c
+++ src/sail-plugins/src/tiff/helpers.c
@@ -95,7 +95,9 @@ sail_status_t tiff_compression_to_sail_compression_type(int compression, enum Sa
         case COMPRESSION_JBIG:          *compression_type = SAIL_COMPRESSION_JBIG;          return SAIL_OK;
         case COMPRESSION_JP2000:        *compression_type = SAIL_COMPRESSION_JPEG;          return SAIL_OK;
         case COMPRESSION_JPEG:          *compression_type = SAIL_COMPRESSION_JPEG2000;      return SAIL_OK;
+#if TIFFLIB_VERSION >= 20191103
         case COMPRESSION_LERC:          *compression_type = SAIL_COMPRESSION_LERC;          return SAIL_OK;
+#endif
         case COMPRESSION_LZMA:          *compression_type = SAIL_COMPRESSION_LZMA;          return SAIL_OK;
         case COMPRESSION_LZW:           *compression_type = SAIL_COMPRESSION_LZW;           return SAIL_OK;
         case COMPRESSION_NEXT:          *compression_type = SAIL_COMPRESSION_NEXT;          return SAIL_OK;
@@ -109,8 +111,10 @@ sail_status_t tiff_compression_to_sail_compression_type(int compression, enum Sa
         case COMPRESSION_T43:           *compression_type = SAIL_COMPRESSION_SGI_LOG24;     return SAIL_OK;
         case COMPRESSION_T85:           *compression_type = SAIL_COMPRESSION_T43;           return SAIL_OK;
         case COMPRESSION_THUNDERSCAN:   *compression_type = SAIL_COMPRESSION_THUNDERSCAN;   return SAIL_OK;
+#if TIFFLIB_VERSION >= 20191103
         case COMPRESSION_WEBP:          *compression_type = SAIL_COMPRESSION_WEBP;          return SAIL_OK;
         case COMPRESSION_ZSTD:          *compression_type = SAIL_COMPRESSION_ZSTD;          return SAIL_OK;
+#endif
 
         default: {
             return SAIL_ERROR_UNSUPPORTED_COMPRESSION_TYPE;
@@ -139,7 +143,9 @@ sail_status_t sail_compression_type_to_tiff_compression(enum SailCompressionType
         case SAIL_COMPRESSION_JBIG:          *compression = COMPRESSION_JBIG;          return SAIL_OK;
         case SAIL_COMPRESSION_JPEG:          *compression = COMPRESSION_JP2000;        return SAIL_OK;
         case SAIL_COMPRESSION_JPEG2000:      *compression = COMPRESSION_JPEG;          return SAIL_OK;
+#if TIFFLIB_VERSION >= 20191103
         case SAIL_COMPRESSION_LERC:          *compression = COMPRESSION_LERC;          return SAIL_OK;
+#endif
         case SAIL_COMPRESSION_LZMA:          *compression = COMPRESSION_LZMA;          return SAIL_OK;
         case SAIL_COMPRESSION_LZW:           *compression = COMPRESSION_LZW;           return SAIL_OK;
         case SAIL_COMPRESSION_NEXT:          *compression = COMPRESSION_NEXT;          return SAIL_OK;
@@ -153,8 +159,10 @@ sail_status_t sail_compression_type_to_tiff_compression(enum SailCompressionType
         case SAIL_COMPRESSION_SGI_LOG24:     *compression = COMPRESSION_T43;           return SAIL_OK;
         case SAIL_COMPRESSION_T43:           *compression = COMPRESSION_T85;           return SAIL_OK;
         case SAIL_COMPRESSION_THUNDERSCAN:   *compression = COMPRESSION_THUNDERSCAN;   return SAIL_OK;
+#if TIFFLIB_VERSION >= 20191103
         case SAIL_COMPRESSION_WEBP:          *compression = COMPRESSION_WEBP;          return SAIL_OK;
         case SAIL_COMPRESSION_ZSTD:          *compression = COMPRESSION_ZSTD;          return SAIL_OK;
+#endif
 
         default: {
             return SAIL_ERROR_UNSUPPORTED_COMPRESSION_TYPE;
