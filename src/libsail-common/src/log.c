d6182c70067d31ccb1b76655f652a810a9d4bc7c
Dmitry Baryshev <dmitrymq@gmail.com>
2020-10-05 18:41:12 +0300

    COMMON: Fix compilation warning

--- src/libsail-common/src/log.c
+++ src/libsail-common/src/log.c
@@ -121,7 +121,7 @@ void sail_log(enum SailLogLevel level, const char *file, int line, const char *f
 
     switch (level) {
         /* Something weird. */
-        case SAIL_LOG_LEVEL_SILENCE: return;
+        case SAIL_LOG_LEVEL_SILENCE: break;
 
         /* Normal log levels. */
         case SAIL_LOG_LEVEL_ERROR:   level_string = "E"; break;
@@ -135,6 +135,8 @@ void sail_log(enum SailLogLevel level, const char *file, int line, const char *f
 
     if (ansi_colors_supported) {
         switch (level) {
+            case SAIL_LOG_LEVEL_SILENCE: break;
+
             case SAIL_LOG_LEVEL_ERROR:   fprintf(SAIL_LOG_FPTR, "%s", SAIL_COLOR_BOLD_RED);    break;
             case SAIL_LOG_LEVEL_WARNING: fprintf(SAIL_LOG_FPTR, "%s", SAIL_COLOR_BOLD_YELLOW); break;
             case SAIL_LOG_LEVEL_INFO:    fprintf(SAIL_LOG_FPTR, "%s", SAIL_COLOR_BOLD_CYAN);   break;
