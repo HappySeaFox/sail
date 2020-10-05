3c95bcfd5c4e443974c2a6a14e44af7ac82e4b87
Dmitry Baryshev <dmitrymq@gmail.com>
2020-10-05 18:14:40 +0300

    COMMON: Fix Coverity Scan warning on continuing loop on error

--- src/libsail/src/sail_private.c
+++ src/libsail/src/sail_private.c
@@ -344,8 +344,9 @@ static sail_status_t init_context(struct sail_context *context, int flags) {
         char *full_path;
 
         /* Ignore errors and try to load as much as possible. */
-        SAIL_TRY_OR_EXECUTE(build_full_path(plugs_path, data.cFileName, &full_path),
-                            /* on error */ continue);
+        if (build_full_path(plugs_path, data.cFileName, &full_path) != SAIL_OK) {
+            continue;
+        }
 
         SAIL_LOG_DEBUG("Found codec info '%s'", data.cFileName);
 
@@ -378,8 +379,9 @@ static sail_status_t init_context(struct sail_context *context, int flags) {
         char *full_path;
 
         /* Ignore errors and try to load as much as possible. */
-        SAIL_TRY_OR_EXECUTE(build_full_path(plugs_path, dir->d_name, &full_path),
-                            /* on error */ continue);
+        if (build_full_path(plugs_path, dir->d_name, &full_path) != SAIL_OK) {
+            continue;
+        }
 
         /* Handle files only. */
         if (sail_is_file(full_path)) {
