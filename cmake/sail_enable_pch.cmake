d7bd94b9aaf54a0f0ab891b3642cbacd7aebc10c
Dmitry Baryshev <dmitrymq@gmail.com>
2020-10-08 17:28:18 +0300

    SAIL: Move the PCH code into a macro

---
+++ cmake/sail_enable_pch.cmake
@@ -0,0 +1,11 @@
+# Intended to be included by SAIL libraries to enable precompiled headers with CMake >= 3.16
+#
+macro(sail_enable_pch)
+    cmake_parse_arguments(SAIL_PCH "" "TARGET" "HEADER" ${ARGN})
+
+    # Enable precompiled headers on the specified target
+    #
+    if (${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.16.0")
+        target_precompile_headers("${SAIL_PCH_TARGET}" PRIVATE "${SAIL_PCH_HEADER}")
+    endif()
+endmacro()
