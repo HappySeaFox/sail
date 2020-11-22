b63711fd58ffe72f1d9644c71e50e8e2e2ca9f3c
Dmitry Baryshev <dmitrymq@gmail.com>
2020-11-22 17:00:55 +0300

    LIBSAIL: Bring the old implementation of strncpy0 back is it's much faster

--- src/libsail/src/ini.c
+++ src/libsail/src/ini.c
@@ -88,11 +88,9 @@ static char* find_chars_or_comment(const char* s, const char* chars)
    NUL-terminated, and doesn't pad with NULs. */
 static char* strncpy0(char* dest, const char* src, size_t size)
 {
-    /* Could use strncpy internally, but it causes gcc warnings (see issue #91) */
-    size_t i;
-    for (i = 0; i < size - 1 && src[i]; i++)
-        dest[i] = src[i];
-    dest[i] = '\0';
+    strncpy(dest, src, size - 1);
+    dest[size - 1] = '\0';
+
     return dest;
 }
 
