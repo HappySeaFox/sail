1106dd1cdc312969476fd3ee7322655dd13727ba
Dmitry Baryshev <dmitrymq@gmail.com>
2020-12-13 22:40:21 +0300

    COMMON: Include missing config.h

--- src/libsail-common/export.h
+++ src/libsail-common/export.h
@@ -26,6 +26,12 @@
 #ifndef SAIL_EXPORT_H
 #define SAIL_EXPORT_H
 
+#ifdef SAIL_BUILD
+    #include "config.h"
+#else
+    #include <sail-common/config.h>
+#endif
+
 #if defined _WIN32 || defined __CYGWIN__
     #ifdef SAIL_STATIC
         #define SAIL_IMPORT
