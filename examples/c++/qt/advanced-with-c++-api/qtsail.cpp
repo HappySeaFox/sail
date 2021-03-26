a2540bca43c0a8ec164f5fcc06a628f923144298
Dmitry Baryshev <dmitrymq@gmail.com>
2021-03-26 10:19:22 +0300

    EXAMPLES: Use CMake in advanced demos

--- examples/c++/qt/advanced-with-c++-api/qtsail.cpp
+++ examples/c++/qt/advanced-with-c++-api/qtsail.cpp
@@ -58,7 +58,7 @@ sail_status_t QtSail::init()
     QTimer::singleShot(0, this, [&]{
         QMessageBox::information(this, tr("Features"), tr("This demo includes:"
                                                           "<ul>"
-                                                          "<li>Linking against SAIL pkg-config packages</li>"
+                                                          "<li>Linking against SAIL CMake packages</li>"
                                                           "<li>Playing animations</li>"
                                                           "</ul>"));
     });
