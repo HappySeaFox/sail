9d39faa28fbbc671737614770189cc933584420e
Dmitry Baryshev <dmitrymq@gmail.com>
2021-04-05 21:33:13 +0300

    BINDINGS/C++: Fixed copying meta data node

--- src/bindings/c++/meta_data-c++.cpp
+++ src/bindings/c++/meta_data-c++.cpp
@@ -242,17 +242,19 @@ sail_status_t meta_data::to_sail_meta_data_node(sail_meta_data_node **md) const
     md_local->value_type = d->value_type;
 
     if (d->value_type == SAIL_META_DATA_TYPE_STRING) {
+        md_local->value_length = d->value_string.length() + 1;
+
         void *ptr;
         SAIL_TRY(sail_memdup(d->value_string.c_str(), md_local->value_length, &ptr));
 
         md_local->value = ptr;
-        md_local->value_length = d->value_string.length() + 1;
     } else {
+        md_local->value_length = d->value_data.size();
+
         void *ptr;
         SAIL_TRY(sail_memdup(d->value_data.data(), md_local->value_length, &ptr));
 
         md_local->value = ptr;
-        md_local->value_length = d->value_data.size();
     }
 
     *md = md_local;
