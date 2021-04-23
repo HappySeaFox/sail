782492f02d42d6ac31b6ad53da98b8c659ae5b37
Dmitry Baryshev <dmitrymq@gmail.com>
2021-04-23 18:12:47 +0300

    BINDINGS/C++: Use sail_conversion_options*

--- src/bindings/c++/conversion_options-c++.cpp
+++ src/bindings/c++/conversion_options-c++.cpp
@@ -38,13 +38,16 @@ class SAIL_HIDDEN conversion_options::pimpl
 {
 public:
     pimpl()
+        : conversion_options(nullptr)
     {
-        conversion_options.options      = 0;
-        conversion_options.background48 = {0, 0, 0};
-        conversion_options.background24 = {0, 0, 0};
+        SAIL_TRY_OR_SUPPRESS(sail_alloc_conversion_options(&conversion_options));
+    }
+    ~pimpl()
+    {
+        sail_destroy_conversion_options(conversion_options);
     }
 
-    sail_conversion_options conversion_options;
+    sail_conversion_options *conversion_options;
 };
 
 conversion_options::conversion_options()
@@ -89,34 +92,34 @@ conversion_options::~conversion_options()
 
 int conversion_options::options() const
 {
-    return d->conversion_options.options;
+    return d->conversion_options->options;
 }
 
 sail_rgb48_t conversion_options::background48() const
 {
-    return d->conversion_options.background48;
+    return d->conversion_options->background48;
 }
 
 sail_rgb24_t conversion_options::background24() const
 {
-    return d->conversion_options.background24;
+    return d->conversion_options->background24;
 }
 
 conversion_options& conversion_options::with_options(int options)
 {
-    d->conversion_options.options = options;
+    d->conversion_options->options = options;
     return *this;
 }
 
 conversion_options& conversion_options::with_background(const sail_rgb48_t &rgb48)
 {
-    d->conversion_options.background48 = rgb48;
+    d->conversion_options->background48 = rgb48;
     return *this;
 }
 
 conversion_options& conversion_options::with_background(const sail_rgb24_t &rgb24)
 {
-    d->conversion_options.background24 = rgb24;
+    d->conversion_options->background24 = rgb24;
     return *this;
 }
 
@@ -125,7 +128,7 @@ sail_status_t conversion_options::to_sail_conversion_options(sail_conversion_opt
     SAIL_CHECK_CONVERSION_OPTIONS_PTR(conversion_options);
 
     SAIL_TRY(sail_alloc_conversion_options(conversion_options));
-    **conversion_options = d->conversion_options;
+    **conversion_options = *d->conversion_options;
 
     return SAIL_OK;
 }
