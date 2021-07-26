2a5600d1246b7630dae7d48ee348a86f3e24bf87
Dmitry Baryshev <dmitrymq@gmail.com>
2021-07-26 22:45:04 +0300

    WEBP: Use demuxer to decode animations (WIP)

--- src/sail-codecs/webp/webp.c
+++ src/sail-codecs/webp/webp.c
@@ -29,6 +29,7 @@
 #include <stdlib.h>
 
 #include <webp/decode.h>
+#include <webp/demux.h>
 
 #include "sail-common.h"
 
@@ -44,6 +45,12 @@ struct webp_state {
 
     WebPDecoderConfig webp_config;
     WebPIDecoder *webp_decoder;
+    WebPDemuxer *webp_demux;
+    WebPIterator *webp_iterator;
+    unsigned frame_number;
+
+    void *image_data;
+    size_t image_data_size;
 };
 
 static sail_status_t alloc_webp_state(struct webp_state **webp_state) {
@@ -55,7 +62,13 @@ static sail_status_t alloc_webp_state(struct webp_state **webp_state) {
     (*webp_state)->read_options  = NULL;
     (*webp_state)->write_options = NULL;
 
-    (*webp_state)->webp_decoder = NULL;
+    (*webp_state)->webp_decoder  = NULL;
+    (*webp_state)->webp_demux    = NULL;
+    (*webp_state)->webp_iterator = NULL;
+    (*webp_state)->frame_number  = 0;
+
+    (*webp_state)->image_data      = NULL;
+    (*webp_state)->image_data_size = 0;
 
     return SAIL_OK;
 }
@@ -65,6 +78,13 @@ static void destroy_webp_state(struct webp_state *webp_state) {
     if (webp_state == NULL) {
         return;
     }
+
+    if (webp_state->webp_iterator != NULL) {
+        WebPDemuxReleaseIterator(webp_state->webp_iterator);
+        sail_free(webp_state->webp_iterator);
+    }
+
+    sail_free(webp_state->image_data);
 
     WebPIDelete(webp_state->webp_decoder);
     WebPFreeDecBuffer(&webp_state->webp_config.output);
@@ -95,13 +115,70 @@ SAIL_EXPORT sail_status_t sail_codec_read_init_v5_webp(struct sail_io *io, const
     /* Deep copy read options. */
     SAIL_TRY(sail_copy_read_options(read_options, &webp_state->read_options));
 
+    /* Read the entire image. */
+    char signature_and_size[8];
+    SAIL_TRY(io->strict_read(io->stream, signature_and_size, sizeof(signature_and_size)));
+    webp_state->image_data_size = *(uint32_t *)(signature_and_size + 4) + sizeof(signature_and_size);
+
+    SAIL_TRY(io->seek(io->stream, SEEK_SET, 0));
+
+    void *ptr;
+    SAIL_TRY(sail_malloc(webp_state->image_data_size, &ptr));
+    webp_state->image_data = ptr;
+
+    SAIL_TRY(io->strict_read(io->stream, webp_state->image_data, webp_state->image_data_size));
+
+    /* Construct a WebP demuxer. */
+    WebPData data = { webp_state->image_data, webp_state->image_data_size };
+
+    webp_state->webp_demux = WebPDemux(&data);
+
+    SAIL_TRY(sail_malloc(sizeof(WebPIterator), &ptr));
+    webp_state->webp_iterator = ptr;
+
+    /* Start demuxing. */
+    if (WebPDemuxGetFrame(webp_state->webp_demux, 1, webp_state->webp_iterator) == 0) {
+        SAIL_LOG_ERROR("WEBP: Failed to get the first frame");
+        SAIL_LOG_AND_RETURN(SAIL_ERROR_UNDERLYING_CODEC);
+    }
+
+#if 0
     if (WebPInitDecoderConfig(&webp_state->webp_config) == 0) {
         SAIL_LOG_ERROR("WEBP: Failed to initialize WebP configuration");
         SAIL_LOG_AND_RETURN(SAIL_ERROR_UNDERLYING_CODEC);
     }
 
-    webp_state->webp_config.options.use_threads = 1;
-    webp_state->webp_config.output.colorspace   = MODE_RGBA;
+    webp_state->webp_config.options.use_threads       = 1;
+    webp_state->webp_config.output.colorspace         = MODE_RGBA;
+    webp_state->webp_config.output.u.RGBA.rgba        = memory_buffer;
+    webp_state->webp_config.output.u.RGBA.stride      = scanline_stride;
+    webp_state->webp_config.output.u.RGBA.size        = total_size_of_the_memory_buffer;
+    webp_state->webp_config.output.is_external_memory = 1;
+
+    webp_state->webp_decoder = WebPINewDecoder(&webp_state->webp_config.output);
+
+    if (webp_state->webp_decoder == NULL) {
+        SAIL_LOG_ERROR("WEBP: Failed to initialize WebP decoder");
+        SAIL_LOG_AND_RETURN(SAIL_ERROR_UNDERLYING_CODEC);
+    }
+
+    size_t actually_read;
+    char buffer[4096];
+
+    do {
+        SAIL_TRY(io->tolerant_read(io->stream, buffer, sizeof(buffer), &actually_read));
+
+        VP8StatusCode status = WebPIAppend(idec, new_data, new_data_size);
+
+        if (status != VP8_STATUS_OK && status != VP8_STATUS_SUSPENDED) {
+            break;
+        }
+
+        WEBP_EXTERN uint8_t* WebPIDecGetRGB(
+            const WebPIDecoder* idec, int* last_y,
+            int* width, int* height, int* stride);
+    } while (actually_read != 0);
+#endif
 
     return SAIL_OK;
 }
@@ -114,50 +191,33 @@ SAIL_EXPORT sail_status_t sail_codec_read_seek_next_frame_v5_webp(void *state, s
 
     struct webp_state *webp_state = (struct webp_state *)state;
 
+    if (webp_state->frame_number > 0) {
+        if (WebPDemuxNextFrame(webp_state->webp_iterator) == 0) {
+            SAIL_LOG_AND_RETURN(SAIL_ERROR_NO_MORE_FRAMES);
+        }
+    }
+
+    webp_state->frame_number++;
+
     struct sail_image *image_local;
     SAIL_TRY(sail_alloc_image(&image_local));
     SAIL_TRY_OR_CLEANUP(sail_alloc_source_image(&image_local->source_image),
                         /* cleanup */ sail_destroy_image(image_local));
 
-#if 0
-    webp_state->webp_config.output.u.RGBA.rgba        = memory_buffer;
-    webp_state->webp_config.output.u.RGBA.stride      = scanline_stride;
-    webp_state->webp_config.output.u.RGBA.size        = total_size_of_the_memory_buffer;
-    webp_state->webp_config.output.is_external_memory = 1;
-    webp_state->webp_decoder = WebPINewDecoder(&webp_state->webp_config.output);
+    image_local->source_image->pixel_format = webp_state->webp_iterator->has_alpha ? SAIL_PIXEL_FORMAT_BPP32_YUVA : SAIL_PIXEL_FORMAT_BPP24_YUV;
+    image_local->source_image->chroma_subsampling = SAIL_CHROMA_SUBSAMPLING_420;
 
-    if (webp_state->webp_decoder == NULL) {
-        SAIL_LOG_ERROR("WEBP: Failed to initialize WebP decoder");
-        SAIL_LOG_AND_RETURN(SAIL_ERROR_UNDERLYING_CODEC);
+    image_local->width = webp_state->webp_iterator->width;
+    image_local->height = webp_state->webp_iterator->height;
+    image_local->pixel_format = SAIL_PIXEL_FORMAT_BPP32_RGBA;
+    if (webp_state->webp_iterator->num_frames > 1) {
+        image_local->delay = webp_state->webp_iterator->duration;
     }
-
-    while (additional_data_is_available) {
-        // ... (get additional data in some new_data[] buffer)
-        VP8StatusCode status = WebPIAppend(idec, new_data, new_data_size);
-
-        if (status != VP8_STATUS_OK && status != VP8_STATUS_SUSPENDED) {
-            break;
-        }
-
-        WEBP_EXTERN uint8_t* WebPIDecGetRGB(
-            const WebPIDecoder* idec, int* last_y,
-            int* width, int* height, int* stride);
-    }
-#endif
-
-#if 0
-    image_local->source_image->pixel_format =
-        webp_private_sail_pixel_format(webp_image->yuvFormat, webp_image->depth, webp_image->alphaPlane != NULL);
-    image_local->source_image->chroma_subsampling = webp_private_sail_chroma_subsampling(webp_image->yuvFormat);
-
-    image_local->width = webp_image->width;
-    image_local->height = webp_image->height;
-    image_local->pixel_format = webp_private_rgb_sail_pixel_format(webp_state->rgb_image.format, webp_state->rgb_image.depth);
-    image_local->delay = (int)(webp_state->webp_decoder->imageTiming.duration * 1000);
 
     SAIL_TRY_OR_CLEANUP(sail_bytes_per_line(image_local->width, image_local->pixel_format, &image_local->bytes_per_line),
                         /* cleanup */ sail_destroy_image(image_local));
 
+#if 0
     /* Fetch ICC profile. */
     if (webp_state->read_options->io_options & SAIL_IO_OPTION_ICCP) {
         SAIL_TRY_OR_CLEANUP(webp_private_fetch_iccp(&webp_image->icc, &image_local->iccp),
@@ -186,6 +246,12 @@ SAIL_EXPORT sail_status_t sail_codec_read_frame_v5_webp(void *state, struct sail
     SAIL_TRY(sail_check_image_skeleton_valid(image));
 
     struct webp_state *webp_state = (struct webp_state *)state;
+
+    if (WebPDecodeRGBAInto(webp_state->webp_iterator->fragment.bytes, webp_state->webp_iterator->fragment.size, image->pixels,
+            image->bytes_per_line * image->height, image->bytes_per_line) == NULL) {
+        SAIL_LOG_ERROR("WEBP: Failed to decode image");
+        SAIL_LOG_AND_RETURN(SAIL_ERROR_UNDERLYING_CODEC);
+    }
 
     return SAIL_OK;
 }
--- src/sail-codecs/webp/webp.cmake
+++ src/sail-codecs/webp/webp.cmake
@@ -1,19 +1,23 @@
 macro(sail_find_dependencies)
     find_library(WEBP_LIBRARY webp)
+    find_library(WEBP_DEMUX_LIBRARY webpdemux)
     find_path(WEBP_INCLUDE_DIRS webp/decode.h)
 
-    if (NOT WEBP_LIBRARY OR NOT WEBP_INCLUDE_DIRS)
+    if (NOT WEBP_LIBRARY OR NOT WEBP_DEMUX_LIBRARY OR NOT WEBP_INCLUDE_DIRS)
         return()
     endif()
 
     set(sail_webp_include_dirs ${WEBP_INCLUDE_DIRS})
-    set(sail_webp_libs ${WEBP_LIBRARY})
+    set(sail_webp_libs ${WEBP_LIBRARY} ${WEBP_DEMUX_LIBRARY})
 
     # This will add the following CMake rules to the CMake config for static builds so a client
     # application links against the required dependencies:
     #
     # find_dependency(WebP REQUIRED)
     # set_property(TARGET SAIL::sail-codecs APPEND PROPERTY INTERFACE_LINK_LIBRARIES ${WEBP_LIBRARY})
+    # find_dependency(WebP REQUIRED)
+    # set_property(TARGET SAIL::sail-codecs APPEND PROPERTY INTERFACE_LINK_LIBRARIES ${WEBP_DEMUX_LIBRARY})
     #
     set(SAIL_CODECS_FIND_DEPENDENCIES ${SAIL_CODECS_FIND_DEPENDENCIES} "WebP,${WEBP_LIBRARY}" PARENT_SCOPE)
+    set(SAIL_CODECS_FIND_DEPENDENCIES ${SAIL_CODECS_FIND_DEPENDENCIES} "WebP,${WEBP_DEMUX_LIBRARY}" PARENT_SCOPE)
 endmacro()
