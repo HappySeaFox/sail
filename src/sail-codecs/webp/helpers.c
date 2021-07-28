be04d4cf21ccbe7f746ec5bfb422d72408b87510
Dmitry Baryshev <dmitrymq@gmail.com>
2021-07-28 16:34:16 +0300

    WEBP: Align

--- src/sail-codecs/webp/helpers.c
+++ src/sail-codecs/webp/helpers.c
@@ -99,7 +99,8 @@ sail_status_t webp_private_fetch_meta_data(WebPDemuxer *webp_demux, struct sail_
         if (WebPDemuxGetChunk(webp_demux, "XMP ", 1, &chunk_iterator)) {
             struct sail_meta_data_node *meta_data_node;
 
-            SAIL_TRY_OR_CLEANUP(sail_alloc_meta_data_node_from_known_substring(SAIL_META_DATA_XMP, (const char *)chunk_iterator.chunk.bytes, chunk_iterator.chunk.size, &meta_data_node),
+            SAIL_TRY_OR_CLEANUP(sail_alloc_meta_data_node_from_known_substring(SAIL_META_DATA_XMP, (const char *)chunk_iterator.chunk.bytes,
+                                chunk_iterator.chunk.size, &meta_data_node),
                         /* cleanup */ WebPDemuxReleaseChunkIterator(&chunk_iterator));
             WebPDemuxReleaseChunkIterator(&chunk_iterator);
 
@@ -114,7 +115,8 @@ sail_status_t webp_private_fetch_meta_data(WebPDemuxer *webp_demux, struct sail_
         if (WebPDemuxGetChunk(webp_demux, "EXIF", 1, &chunk_iterator)) {
             struct sail_meta_data_node *meta_data_node;
 
-            SAIL_TRY_OR_CLEANUP(sail_alloc_meta_data_node_from_known_data(SAIL_META_DATA_EXIF, chunk_iterator.chunk.bytes, chunk_iterator.chunk.size, &meta_data_node),
+            SAIL_TRY_OR_CLEANUP(sail_alloc_meta_data_node_from_known_data(SAIL_META_DATA_EXIF, chunk_iterator.chunk.bytes,
+                                chunk_iterator.chunk.size, &meta_data_node),
                         /* cleanup */ WebPDemuxReleaseChunkIterator(&chunk_iterator));
             WebPDemuxReleaseChunkIterator(&chunk_iterator);
 
