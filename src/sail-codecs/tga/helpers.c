096766f519eb714f4219565a7106db5b9f3e1a93
Dmitry Baryshev <dmitrymq@gmail.com>
2021-09-16 20:26:57 +0300

    TGA: First version with reading file header

---
+++ src/sail-codecs/tga/helpers.c
@@ -0,0 +1,182 @@
+/*  This file is part of SAIL (https://github.com/smoked-herring/sail)
+
+    Copyright (c) 2021 Dmitry Baryshev
+
+    The MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#include <stdio.h>
+
+#include "sail-common.h"
+
+#include "helpers.h"
+
+sail_status_t tga_private_read_file_header(struct sail_io *io, struct TgaFileHeader *file_header) {
+
+    SAIL_TRY(io->strict_read(io->stream, &file_header->id_length,                   sizeof(file_header->id_length)));
+    SAIL_TRY(io->strict_read(io->stream, &file_header->color_map_type,              sizeof(file_header->color_map_type)));
+    SAIL_TRY(io->strict_read(io->stream, &file_header->image_type,                  sizeof(file_header->image_type)));
+    SAIL_TRY(io->strict_read(io->stream, &file_header->first_color_map_entry_index, sizeof(file_header->first_color_map_entry_index)));
+    SAIL_TRY(io->strict_read(io->stream, &file_header->color_map_elements,          sizeof(file_header->color_map_elements)));
+    SAIL_TRY(io->strict_read(io->stream, &file_header->color_map_entry_size,        sizeof(file_header->color_map_entry_size)));
+    SAIL_TRY(io->strict_read(io->stream, &file_header->x,                           sizeof(file_header->x)));
+    SAIL_TRY(io->strict_read(io->stream, &file_header->y,                           sizeof(file_header->y)));
+    SAIL_TRY(io->strict_read(io->stream, &file_header->width,                       sizeof(file_header->width)));
+    SAIL_TRY(io->strict_read(io->stream, &file_header->height,                      sizeof(file_header->height)));
+    SAIL_TRY(io->strict_read(io->stream, &file_header->bpp,                         sizeof(file_header->bpp)));
+    SAIL_TRY(io->strict_read(io->stream, &file_header->descriptor,                  sizeof(file_header->descriptor)));
+
+    return SAIL_OK;
+}
+
+enum SailPixelFormat tga_private_sail_pixel_format(int image_type, int bpp) {
+
+    switch (image_type) {
+        case TGA_INDEXED:
+        case TGA_INDEXED_RLE: {
+            return SAIL_PIXEL_FORMAT_BPP8_INDEXED;
+        }
+
+        case TGA_TRUE_COLOR:
+        case TGA_TRUE_COLOR_RLE: {
+            switch (bpp) {
+                case 16: return SAIL_PIXEL_FORMAT_BPP16_RGB555;
+                case 24: return SAIL_PIXEL_FORMAT_BPP24_RGB;
+                case 32: return SAIL_PIXEL_FORMAT_BPP32_ARGB;
+                default: return SAIL_PIXEL_FORMAT_UNKNOWN;
+            }
+        }
+
+        case TGA_MONO:
+        case TGA_MONO_RLE: {
+            return SAIL_PIXEL_FORMAT_BPP1_INDEXED;
+        }
+
+        default: {
+            return SAIL_PIXEL_FORMAT_UNKNOWN;
+        }
+    }
+}
+
+enum SailPixelFormat tga_private_palette_bpp_to_sail_pixel_format(int bpp) {
+
+    switch (bpp) {
+        case 15:
+        case 16:
+        case 24: return SAIL_PIXEL_FORMAT_BPP24_RGB;
+        case 32: return SAIL_PIXEL_FORMAT_BPP32_RGBA;
+        default: {
+            SAIL_LOG_ERROR("TGA: Palette bit depth %d is not supported", bpp);
+            return SAIL_PIXEL_FORMAT_UNKNOWN;
+        }
+    }
+}
+
+sail_status_t tga_private_fetch_id(struct sail_io *io, const struct TgaFileHeader *file_header, struct sail_meta_data_node **meta_data_node) {
+
+    struct sail_meta_data_node *meta_data_node_local;
+
+    SAIL_TRY(sail_alloc_meta_data_node_and_value(&meta_data_node_local));
+
+    struct sail_meta_data *meta_data = meta_data_node_local->meta_data;
+
+    meta_data->key          = SAIL_META_DATA_ID;
+    meta_data->value_type   = SAIL_META_DATA_TYPE_STRING;
+    meta_data->value_length = file_header->id_length + 1;
+
+    SAIL_TRY_OR_CLEANUP(sail_malloc(meta_data->value_length, &meta_data->value),
+                        /* cleanup */ sail_destroy_meta_data_node(meta_data_node_local));
+    SAIL_TRY_OR_CLEANUP(io->strict_read(io->stream, meta_data->value, meta_data->value_length - 1),
+                        /* cleanup */ sail_destroy_meta_data_node(meta_data_node_local));
+
+    ((char *)meta_data->value)[meta_data->value_length - 1] = '\0';
+
+    *meta_data_node = meta_data_node_local;
+
+    return SAIL_OK;
+}
+
+sail_status_t tga_private_fetch_palette(struct sail_io *io, const struct TgaFileHeader *file_header, struct sail_palette **palette) {
+
+    size_t element_size_in_bytes = ((size_t)file_header->color_map_entry_size + 7) / 8;
+    size_t bytes_to_skip = file_header->first_color_map_entry_index * element_size_in_bytes;
+
+    if (bytes_to_skip > 0) {
+        SAIL_TRY(io->seek(io->stream, (long)bytes_to_skip, SEEK_CUR));
+    }
+
+    enum SailPixelFormat palette_pixel_format = tga_private_palette_bpp_to_sail_pixel_format(file_header->color_map_entry_size);
+
+    if (palette_pixel_format == SAIL_PIXEL_FORMAT_UNKNOWN) {
+        SAIL_LOG_AND_RETURN(SAIL_ERROR_UNSUPPORTED_PIXEL_FORMAT);
+    }
+
+    unsigned palette_elements = (unsigned)file_header->color_map_elements - file_header->first_color_map_entry_index;
+    struct sail_palette *palette_local;
+
+    SAIL_TRY(sail_alloc_palette_for_data(palette_pixel_format, palette_elements, &palette_local));
+
+    unsigned char *palette_data = palette_local->data;
+
+    for (int i = file_header->first_color_map_entry_index; i < file_header->color_map_elements; i++) {
+        unsigned char data[4];
+
+        SAIL_TRY_OR_CLEANUP(io->strict_read(io->stream, data, element_size_in_bytes),
+                            /* cleanup */ sail_destroy_palette(palette_local));
+
+        switch (file_header->color_map_entry_size) {
+            case 15:
+            case 16: {
+                const uint16_t *data_as_word = (const uint16_t *)data;
+
+                *palette_data++ = (unsigned char)(((*data_as_word & 0x1f)   <<  3) << 0);
+                *palette_data++ = (unsigned char)(((*data_as_word & 0x3e0)  >>  5) << 3);
+                *palette_data++ = (unsigned char)(((*data_as_word & 0x7c00) >> 10) << 3);
+                break;
+            }
+
+            case 24: {
+                *palette_data++ = data[0];
+                *palette_data++ = data[1];
+                *palette_data++ = data[2];
+                break;
+            }
+
+            case 32: {
+                *palette_data++ = data[1];
+                *palette_data++ = data[2];
+                *palette_data++ = data[3];
+                *palette_data++ = data[0];
+                break;
+            }
+
+            default: {
+                SAIL_LOG_ERROR("TGA: Internal error: Unhandled palette pixel format");
+                sail_destroy_palette(palette_local);
+                SAIL_LOG_AND_RETURN(SAIL_ERROR_UNSUPPORTED_PIXEL_FORMAT);
+            }
+        }
+    }
+
+    *palette = palette_local;
+
+    return SAIL_OK;
+}
---
+++ src/sail-codecs/tga/tga.c
@@ -0,0 +1,256 @@
+/*  This file is part of SAIL (https://github.com/smoked-herring/sail)
+
+    Copyright (c) 2021 Dmitry Baryshev
+
+    The MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sail-common.h"
+
+#include "helpers.h"
+
+static const char * const TGA_SIGNATURE = "TRUEVISION-XFILE.";
+
+/*
+ * Codec-specific state.
+ */
+struct tga_state {
+    struct sail_read_options *read_options;
+    struct sail_write_options *write_options;
+
+    struct TgaFileHeader file_header;
+    struct TgaFooter footer;
+
+    bool frame_read;
+    bool tga2;
+};
+
+static sail_status_t alloc_tga_state(struct tga_state **tga_state) {
+
+    void *ptr;
+    SAIL_TRY(sail_malloc(sizeof(struct tga_state), &ptr));
+    *tga_state = ptr;
+
+    (*tga_state)->read_options  = NULL;
+    (*tga_state)->write_options = NULL;
+
+    (*tga_state)->frame_read    = false;
+    (*tga_state)->tga2          = false;
+
+    return SAIL_OK;
+}
+
+static void destroy_tga_state(struct tga_state *tga_state) {
+
+    if (tga_state == NULL) {
+        return;
+    }
+
+    sail_destroy_read_options(tga_state->read_options);
+    sail_destroy_write_options(tga_state->write_options);
+
+    sail_free(tga_state);
+}
+
+/*
+ * Decoding functions.
+ */
+
+SAIL_EXPORT sail_status_t sail_codec_read_init_v5_tga(struct sail_io *io, const struct sail_read_options *read_options, void **state) {
+
+    SAIL_CHECK_PTR(state);
+    *state = NULL;
+
+    SAIL_TRY(sail_check_io_valid(io));
+    SAIL_CHECK_PTR(read_options);
+
+    /* Allocate a new state. */
+    struct tga_state *tga_state;
+    SAIL_TRY(alloc_tga_state(&tga_state));
+    *state = tga_state;
+
+    /* Deep copy read options. */
+    SAIL_TRY(sail_copy_read_options(read_options, &tga_state->read_options));
+
+    SAIL_TRY(io->seek(io->stream, -(long)(sizeof(tga_state->footer.signature)), SEEK_END));
+    SAIL_TRY(io->strict_read(io->stream, tga_state->footer.signature, sizeof(tga_state->footer.signature)));
+
+    tga_state->tga2 = strcmp(TGA_SIGNATURE, tga_state->footer.signature) == 0;
+
+    SAIL_TRY(io->seek(io->stream, 0, SEEK_SET));
+
+    return SAIL_OK;
+}
+
+SAIL_EXPORT sail_status_t sail_codec_read_seek_next_frame_v5_tga(void *state, struct sail_io *io, struct sail_image **image) {
+
+    SAIL_CHECK_PTR(state);
+    SAIL_TRY(sail_check_io_valid(io));
+    SAIL_CHECK_PTR(image);
+
+    struct tga_state *tga_state = (struct tga_state *)state;
+
+    if (tga_state->frame_read) {
+        SAIL_LOG_AND_RETURN(SAIL_ERROR_NO_MORE_FRAMES);
+    }
+
+    tga_state->frame_read = true;
+
+    struct sail_image *image_local;
+    SAIL_TRY(sail_alloc_image(&image_local));
+    SAIL_TRY_OR_CLEANUP(sail_alloc_source_image(&image_local->source_image),
+                        /* cleanup */ sail_destroy_image(image_local));
+
+    SAIL_TRY_OR_CLEANUP(tga_private_read_file_header(io, &tga_state->file_header),
+                        /* cleanup */ sail_destroy_image(image_local));
+
+    image_local->source_image->pixel_format = tga_private_sail_pixel_format(tga_state->file_header.image_type, tga_state->file_header.bpp);
+
+    if (image_local->source_image->pixel_format == SAIL_PIXEL_FORMAT_UNKNOWN) {
+        sail_destroy_image(image_local);
+        SAIL_LOG_AND_RETURN(SAIL_ERROR_UNSUPPORTED_PIXEL_FORMAT);
+    }
+
+    image_local->width = tga_state->file_header.width;
+    image_local->height = tga_state->file_header.height;
+    image_local->pixel_format = image_local->source_image->pixel_format;
+
+    SAIL_TRY_OR_CLEANUP(sail_bytes_per_line(image_local->width, image_local->pixel_format, &image_local->bytes_per_line),
+                        /* cleanup */ sail_destroy_image(image_local));
+
+    /* Identificator. */
+    if (tga_state->file_header.id_length > 0) {
+        SAIL_TRY_OR_CLEANUP(tga_private_fetch_id(io, &tga_state->file_header, &image_local->meta_data_node),
+                            /* cleanup */ sail_destroy_image(image_local));
+    }
+
+    /* Palette. */
+    if (tga_state->file_header.color_map_type == TGA_HAS_COLOR_MAP) {
+        SAIL_TRY_OR_CLEANUP(tga_private_fetch_palette(io, &tga_state->file_header, &image_local->palette),
+                            /* cleanup */ sail_destroy_image(image_local));
+    } else if (tga_state->file_header.image_type == TGA_MONO || tga_state->file_header.image_type == TGA_MONO_RLE) {
+        /* Allocate B&W palette. */
+        SAIL_TRY_OR_CLEANUP(sail_alloc_palette_for_data(SAIL_PIXEL_FORMAT_BPP24_RGB, 2, &image_local->palette),
+                            /* cleanup */ sail_destroy_image(image_local));
+
+        unsigned char *palette_data = image_local->palette->data;
+
+        *palette_data++ = 0;
+        *palette_data++ = 0;
+        *palette_data++ = 0;
+
+        *palette_data++ = 255;
+        *palette_data++ = 255;
+        *palette_data++ = 255;
+    }
+
+    *image = image_local;
+
+    return SAIL_OK;
+}
+
+SAIL_EXPORT sail_status_t sail_codec_read_seek_next_pass_v5_tga(void *state, struct sail_io *io, const struct sail_image *image) {
+
+    SAIL_CHECK_PTR(state);
+    SAIL_TRY(sail_check_io_valid(io));
+    SAIL_TRY(sail_check_image_skeleton_valid(image));
+
+    return SAIL_OK;
+}
+
+SAIL_EXPORT sail_status_t sail_codec_read_frame_v5_tga(void *state, struct sail_io *io, struct sail_image *image) {
+
+    SAIL_CHECK_PTR(state);
+    SAIL_TRY(sail_check_io_valid(io));
+    SAIL_TRY(sail_check_image_skeleton_valid(image));
+
+    struct tga_state *tga_state = (struct tga_state *)state;
+
+    return SAIL_OK;
+}
+
+SAIL_EXPORT sail_status_t sail_codec_read_finish_v5_tga(void **state, struct sail_io *io) {
+
+    SAIL_CHECK_PTR(state);
+    SAIL_TRY(sail_check_io_valid(io));
+
+    struct tga_state *tga_state = (struct tga_state *)(*state);
+
+    /* Subsequent calls to finish() will expectedly fail in the above line. */
+    *state = NULL;
+
+    destroy_tga_state(tga_state);
+
+    return SAIL_OK;
+}
+
+/*
+ * Encoding functions.
+ */
+
+SAIL_EXPORT sail_status_t sail_codec_write_init_v5_tga(struct sail_io *io, const struct sail_write_options *write_options, void **state) {
+
+    SAIL_CHECK_PTR(state);
+    SAIL_TRY(sail_check_io_valid(io));
+    SAIL_CHECK_PTR(write_options);
+
+    SAIL_LOG_AND_RETURN(SAIL_ERROR_NOT_IMPLEMENTED);
+}
+
+SAIL_EXPORT sail_status_t sail_codec_write_seek_next_frame_v5_tga(void *state, struct sail_io *io, const struct sail_image *image) {
+
+    SAIL_CHECK_PTR(state);
+    SAIL_TRY(sail_check_io_valid(io));
+    SAIL_TRY(sail_check_image_valid(image));
+
+    SAIL_LOG_AND_RETURN(SAIL_ERROR_NOT_IMPLEMENTED);
+}
+
+SAIL_EXPORT sail_status_t sail_codec_write_seek_next_pass_v5_tga(void *state, struct sail_io *io, const struct sail_image *image) {
+
+    SAIL_CHECK_PTR(state);
+    SAIL_TRY(sail_check_io_valid(io));
+    SAIL_TRY(sail_check_image_valid(image));
+
+    SAIL_LOG_AND_RETURN(SAIL_ERROR_NOT_IMPLEMENTED);
+}
+
+SAIL_EXPORT sail_status_t sail_codec_write_frame_v5_tga(void *state, struct sail_io *io, const struct sail_image *image) {
+
+    SAIL_CHECK_PTR(state);
+    SAIL_TRY(sail_check_io_valid(io));
+    SAIL_TRY(sail_check_image_valid(image));
+
+    SAIL_LOG_AND_RETURN(SAIL_ERROR_NOT_IMPLEMENTED);
+}
+
+SAIL_EXPORT sail_status_t sail_codec_write_finish_v5_tga(void **state, struct sail_io *io) {
+
+    SAIL_CHECK_PTR(state);
+    SAIL_TRY(sail_check_io_valid(io));
+
+    SAIL_LOG_AND_RETURN(SAIL_ERROR_NOT_IMPLEMENTED);
+}
