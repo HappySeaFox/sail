ca7eea2da4707d356c9cfe569439815ccee80726
Dmitry Baryshev <dmitrymq@gmail.com>
2021-09-23 16:31:22 +0300

    COMMON: Added SAIL_META_DATA_ID serialize

--- src/libsail-common/common_serialize.c
+++ src/libsail-common/common_serialize.c
@@ -353,6 +353,7 @@ const char* sail_meta_data_to_string(enum SailMetaData meta_data) {
         case SAIL_META_DATA_DISCLAIMER:    return "Disclaimer";
         case SAIL_META_DATA_DOCUMENT:      return "Document";
         case SAIL_META_DATA_EXIF:          return "EXIF";
+        case SAIL_META_DATA_ID:            return "ID";
         case SAIL_META_DATA_IPTC:          return "IPTC";
         case SAIL_META_DATA_LABEL:         return "Label";
         case SAIL_META_DATA_MAKE:          return "Make";
@@ -389,6 +390,7 @@ enum SailMetaData sail_meta_data_from_string(const char *str) {
         case UINT64_C(8244735206874071778):  return SAIL_META_DATA_DISCLAIMER;
         case UINT64_C(7570930199009348):     return SAIL_META_DATA_DOCUMENT;
         case UINT64_C(6384018865):           return SAIL_META_DATA_EXIF;
+        case UINT64_C(5862386):              return SAIL_META_DATA_ID;
         case UINT64_C(6384154261):           return SAIL_META_DATA_IPTC;
         case UINT64_C(210681275781):         return SAIL_META_DATA_LABEL;
         case UINT64_C(6384317315):           return SAIL_META_DATA_MAKE;
