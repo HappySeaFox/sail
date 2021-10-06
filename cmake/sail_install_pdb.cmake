0d11a301b1263dc48742f53d92a0ec771761ad7a
Dmitry Baryshev <dmitrymq@gmail.com>
2021-10-06 22:22:12 +0300

    SAIL: Install PDB files with MSVC only

--- cmake/sail_install_pdb.cmake
+++ cmake/sail_install_pdb.cmake
@@ -3,7 +3,7 @@
 macro(sail_install_pdb)
     cmake_parse_arguments(SAIL_INSTALL_PDB "" "TARGET" "" ${ARGN})
 
-    if (NOT SAIL_STATIC)
+    if (MSVC AND NOT SAIL_STATIC)
         install(FILES $<TARGET_PDB_FILE:${SAIL_INSTALL_PDB_TARGET}> DESTINATION bin OPTIONAL)
     endif()
 endmacro()
