0f659837e9088ee914c2166e1fd5a2e69f54e12a
Dmitry Baryshev <dmitrymq@gmail.com>
2021-10-06 14:49:02 +0300

    SAIL: Install PDB files in debug mode

---
+++ cmake/sail_install_pdb.cmake
@@ -0,0 +1,7 @@
+# Intended to be included by SAIL libraries to install their debug PDB files
+#
+macro(sail_install_pdb)
+    cmake_parse_arguments(SAIL_INSTALL_PDB "" "TARGET" "" ${ARGN})
+
+    install(FILES $<TARGET_PDB_FILE:${SAIL_INSTALL_PDB_TARGET}> DESTINATION bin OPTIONAL)
+endmacro()
