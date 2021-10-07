c48ed6b09b9426b323661756b43df744aa8f7bde
Dmitry Baryshev <dmitrymq@gmail.com>
2021-10-07 19:54:58 +0300

    TESTS: Added test to load images in C++

---
+++ tests/bindings/c++/able-to-load.cpp
@@ -0,0 +1,66 @@
+/*  This file is part of SAIL (https://github.com/smoked-herring/sail)
+
+    Copyright (c) 2020-2021 Dmitry Baryshev
+
+    The MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#include <stdio.h>
+
+#include "sail-c++.h"
+
+#include "munit.h"
+
+#include "test-images.h"
+
+static MunitResult test_able_to_load(const MunitParameter params[], void *user_data) {
+    (void)user_data;
+
+    const char *path = munit_parameters_get(params, "path");
+
+    const sail::image image(path);
+    munit_assert(image.is_valid());
+
+    return MUNIT_OK;
+}
+
+static MunitParameterEnum test_params[] = {
+    { (char *)"path", (char **)SAIL_TEST_IMAGES },
+    { NULL, NULL },
+};
+
+static MunitTest test_suite_tests[] = {
+    { (char *)"/able-to-load", test_able_to_load, NULL, NULL, MUNIT_TEST_OPTION_NONE, test_params },
+
+    { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
+};
+
+static const MunitSuite test_suite = {
+    (char *)"/able-to-load",
+    test_suite_tests,
+    NULL,
+    1,
+    MUNIT_SUITE_OPTION_NONE
+};
+
+int main(int argc, char *argv[MUNIT_ARRAY_PARAM(argc + 1)]) {
+    return munit_suite_main(&test_suite, NULL, argc, argv);
+}
