899899288130292be86351709b28cc98adccfa7c
Dmitry Baryshev <dmitrymq@gmail.com>
2021-12-21 22:13:21 +0300

    SAIL: Added RENAME parameter for sail_install_pdb() to allow sail.exe.pdb

--- cmake/sail_install_pdb.cmake
+++ cmake/sail_install_pdb.cmake
@@ -1,9 +1,13 @@
 # Intended to be included by SAIL libraries to install their debug PDB files
 #
 macro(sail_install_pdb)
-    cmake_parse_arguments(SAIL_INSTALL_PDB "" "TARGET" "" ${ARGN})
+    cmake_parse_arguments(SAIL_INSTALL_PDB "" "TARGET;RENAME" "" ${ARGN})
 
     if (MSVC AND NOT SAIL_STATIC)
-        install(FILES $<TARGET_PDB_FILE:${SAIL_INSTALL_PDB_TARGET}> DESTINATION bin OPTIONAL)
+        if (SAIL_INSTALL_PDB_RENAME)
+            install(FILES $<TARGET_PDB_FILE:${SAIL_INSTALL_PDB_TARGET}> DESTINATION bin RENAME ${SAIL_INSTALL_PDB_RENAME} OPTIONAL)
+        else()
+            install(FILES $<TARGET_PDB_FILE:${SAIL_INSTALL_PDB_TARGET}> DESTINATION bin OPTIONAL)
+        endif()
     endif()
 endmacro()
