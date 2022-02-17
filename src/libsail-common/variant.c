5bd744958c2b42fe32acb17e2683cdad6ac08838
Dmitry Baryshev <dima8w@gmail.com>
2022-02-17 15:50:00 -0800

    COMMON: WIP in variant

---
+++ src/libsail-common/variant.c
@@ -0,0 +1,225 @@
+/*  This file is part of SAIL (https://github.com/smoked-herring/sail)
+
+    Copyright (c) 2020 Dmitry Baryshev
+
+    The MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#include <string.h>
+
+#include "sail-common.h"
+
+/*
+ * Private functions.
+ */
+static sail_status_t alloc_variant(enum SailVariantType value_type, void *value, size_t value_size, struct sail_variant **variant) {
+
+    SAIL_CHECK_PTR(variant);
+
+    struct sail_variant *variant_local;
+    SAIL_TRY(sail_alloc_variant(&variant_local));
+
+    void *ptr;
+    SAIL_TRY_OR_CLEANUP(sail_malloc(value_size, &ptr),
+                        /* on error */ sail_destroy_variant(variant_local));
+    variant_local->value = ptr;
+    memcpy(variant_local->value, value, value_size);
+
+    variant_local->value_type = value_type;
+    variant_local->value_size = value_size;
+
+    *variant = variant_local;
+
+    return SAIL_OK;
+}
+
+/*
+ * Public functions.
+ */
+sail_status_t sail_alloc_variant(struct sail_variant **variant) {
+
+    SAIL_CHECK_PTR(variant);
+
+    void *ptr;
+    SAIL_TRY(sail_malloc(sizeof(struct sail_variant), &ptr));
+    *variant = ptr;
+
+    (*variant)->value_type  = SAIL_META_DATA_UNKNOWN;
+    (*variant)->value       = NULL;
+    (*variant)->value_size  = SAIL_VARIANT_TYPE_INVALID;
+
+    return SAIL_OK;
+}
+
+sail_status_t sail_alloc_variant_from_char(char value, struct sail_variant **variant)
+{
+    SAIL_TRY(alloc_variant(SAIL_VARIANT_TYPE_CHAR, &value, sizeof(value), variant));
+
+    return SAIL_OK;
+}
+
+sail_status_t sail_alloc_variant_from_unsigned_char(unsigned char value, struct sail_variant **variant)
+{
+    SAIL_TRY(alloc_variant(SAIL_VARIANT_TYPE_UNSIGNED_CHAR, &value, sizeof(value), variant));
+
+    return SAIL_OK;
+}
+
+sail_status_t sail_alloc_variant_from_short(short value, struct sail_variant **variant)
+{
+    SAIL_TRY(alloc_variant(SAIL_VARIANT_TYPE_SHORT, &value, sizeof(value), variant));
+
+    return SAIL_OK;
+}
+
+sail_status_t sail_alloc_variant_from_unsigned_short(unsigned short value, struct sail_variant **variant)
+{
+    SAIL_TRY(alloc_variant(SAIL_VARIANT_TYPE_UNSIGNED_SHORT, &value, sizeof(value), variant));
+
+    return SAIL_OK;
+}
+
+sail_status_t sail_alloc_variant_from_int(int value, struct sail_variant **variant)
+{
+    SAIL_TRY(alloc_variant(SAIL_VARIANT_TYPE_INT, &value, sizeof(value), variant));
+
+    return SAIL_OK;
+}
+
+sail_status_t sail_alloc_variant_from_unsigned_int(unsigned int value, struct sail_variant **variant)
+{
+    SAIL_TRY(alloc_variant(SAIL_VARIANT_TYPE_UNSIGNED_INT, &value, sizeof(value), variant));
+
+    return SAIL_OK;
+}
+
+sail_status_t sail_alloc_variant_from_long(long value, struct sail_variant **variant)
+{
+    SAIL_TRY(alloc_variant(SAIL_VARIANT_TYPE_LONG, &value, sizeof(value), variant));
+
+    return SAIL_OK;
+}
+
+sail_status_t sail_alloc_variant_from_unsigned_long(unsigned long value, struct sail_variant **variant)
+{
+    SAIL_TRY(alloc_variant(SAIL_VARIANT_TYPE_UNSIGNED_LONG, &value, sizeof(value), variant));
+
+    return SAIL_OK;
+}
+
+sail_status_t sail_alloc_variant_from_data(void *value, size_t value_size, struct sail_variant **variant)
+{
+    SAIL_TRY(alloc_variant(SAIL_VARIANT_TYPE_DATA, value, value_size, variant));
+
+    return SAIL_OK;
+}
+
+sail_status_t sail_alloc_variant_from_data_pointer(void *value, size_t value_size, struct sail_variant **variant)
+{
+    SAIL_TRY(sail_alloc_variant(variant));
+
+    (*variant)->value_type = SAIL_VARIANT_TYPE_DATA;
+    (*variant)->value      = value;
+    (*variant)->value_size = value_size;
+
+    return SAIL_OK;
+}
+
+void sail_destroy_variant(struct sail_variant *variant)
+{
+    if (variant == NULL) {
+        return;
+    }
+
+    sail_free(variant->value);
+    sail_free(variant);
+}
+
+char sail_variant_to_char(const struct sail_variant *variant)
+{
+    return *(char *)(variant->value);
+}
+
+unsigned char sail_variant_to_unsigned_char(const struct sail_variant *variant)
+{
+    return *(unsigned char *)(variant->value);
+}
+
+short sail_variant_to_short(const struct sail_variant *variant)
+{
+    return *(short *)(variant->value);
+}
+
+unsigned short sail_variant_to_unsigned_short(const struct sail_variant *variant)
+{
+    return *(unsigned short *)(variant->value);
+}
+
+int sail_variant_to_int(const struct sail_variant *variant)
+{
+    return *(int *)(variant->value);
+}
+
+unsigned int sail_variant_to_unsigned_int(const struct sail_variant *variant)
+{
+    return *(unsigned int *)(variant->value);
+}
+
+long sail_variant_to_long(const struct sail_variant *variant)
+{
+    return *(long *)(variant->value);
+}
+
+unsigned long sail_variant_to_unsigned_long(const struct sail_variant *variant)
+{
+    return *(unsigned long *)(variant->value);
+}
+
+char* sail_variant_to_string(const struct sail_variant *variant)
+{
+    return (char *)(variant->value);
+}
+
+void* sail_variant_to_data(const struct sail_variant *variant)
+{
+    return variant->value;
+}
+
+sail_status_t sail_check_variant_valid(const struct sail_variant *variant)
+{
+    SAIL_CHECK_PTR(variant);
+
+    if (variant->value_type == SAIL_VARIANT_TYPE_INVALID || variant->value == NULL || variant->value_size == 0) {
+        SAIL_LOG_AND_RETURN(SAIL_ERROR_INVALID_VARIANT);
+    }
+
+    return SAIL_OK;
+}
+
+sail_status_t sail_copy_variant(const struct sail_variant *source, struct sail_variant **target) {
+
+    SAIL_CHECK_PTR(source);
+    SAIL_CHECK_PTR(target);
+
+    SAIL_TRY(alloc_variant(source->value_type, source->value, source->value_size, target));
+
+    return SAIL_OK;
+}
---
+++ src/libsail-common/variant.h
@@ -0,0 +1,248 @@
+/*  This file is part of SAIL (https://github.com/smoked-herring/sail)
+
+    Copyright (c) 2022 Dmitry Baryshev
+
+    The MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#ifndef SAIL_VARIANT_H
+#define SAIL_VARIANT_H
+
+#include <stddef.h>
+
+#ifdef SAIL_BUILD
+    #include "common.h"
+    #include "error.h"
+    #include "export.h"
+#else
+    #include <sail-common/common.h>
+    #include <sail-common/error.h>
+    #include <sail-common/export.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Type of variant data.
+ */
+enum SailVariantType {
+
+    SAIL_VARIANT_TYPE_CHAR,
+    SAIL_VARIANT_TYPE_UNSIGNED_CHAR,
+    SAIL_VARIANT_TYPE_SHORT,
+    SAIL_VARIANT_TYPE_UNSIGNED_SHORT,
+    SAIL_VARIANT_TYPE_INT,
+    SAIL_VARIANT_TYPE_UNSIGNED_INT,
+    SAIL_VARIANT_TYPE_LONG,
+    SAIL_VARIANT_TYPE_UNSIGNED_LONG,
+    SAIL_VARIANT_TYPE_DATA,
+    SAIL_VARIANT_TYPE_STRING,
+    SAIL_VARIANT_TYPE_USER,
+    SAIL_VARIANT_TYPE_INVALID,
+};
+
+/*
+ * Variant with limited possible data values.
+ */
+struct sail_variant {
+
+    /*
+     * Value type.
+     */
+    enum SailVariantType value_type;
+
+    /*
+     * Actual meta data value. value_length holds its length.
+     */
+    void *value;
+
+    /*
+     * The size of the allocated memory for the value. For strings, it's strlen() + 1.
+     */
+    size_t value_size;
+};
+
+/*
+ * Allocates a new invalid variant without any value.
+ * Use sail_alloc_variant_from_xxx() to allocate variants from actual data.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_alloc_variant(struct sail_variant **variant);
+
+/*
+ * Allocates a new variant from the specified char value.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_alloc_variant_from_char(char value, struct sail_variant **variant);
+
+/*
+ * Allocates a new variant from the specified unsigned char value.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_alloc_variant_from_unsigned_char(unsigned char value, struct sail_variant **variant);
+
+/*
+ * Allocates a new variant from the specified short value.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_alloc_variant_from_short(short value, struct sail_variant **variant);
+
+/*
+ * Allocates a new variant from the specified unsigned short value.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_alloc_variant_from_unsigned_short(unsigned short value, struct sail_variant **variant);
+
+/*
+ * Allocates a new variant from the specified int value.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_alloc_variant_from_int(int value, struct sail_variant **variant);
+
+/*
+ * Allocates a new variant from the specified unsigned int value.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_alloc_variant_from_unsigned_int(unsigned int value, struct sail_variant **variant);
+
+/*
+ * Allocates a new variant from the specified long value.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_alloc_variant_from_long(long value, struct sail_variant **variant);
+
+/*
+ * Allocates a new variant from the specified unsigned long value.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_alloc_variant_from_unsigned_long(unsigned long value, struct sail_variant **variant);
+
+/*
+ * Allocates a new variant from the specified data buffer. Makes a deep copy of the buffer.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_alloc_variant_from_data(void *value, size_t value_size, struct sail_variant **variant);
+
+/*
+ * Allocates a new variant from the specified data buffer pointer. Makes a shallow copy of the buffer pointer.
+ * Transfers the ownership of the pointer to the constructed variant.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_alloc_variant_from_data_pointer(void *value, size_t value_size, struct sail_variant **variant);
+
+/*
+ * Destroys the specified variant.
+ */
+SAIL_EXPORT void sail_destroy_variant(struct sail_variant *variant);
+
+/*
+ * Returns the variant char value. Behavior is undefined if the variant is not valid
+ * or its type is not char.
+ */
+SAIL_EXPORT char sail_variant_to_char(const struct sail_variant *variant);
+
+/*
+ * Returns the variant unsigned char value. Behavior is undefined if the variant is not valid
+ * or its type is not unsigned char.
+ */
+SAIL_EXPORT unsigned char sail_variant_to_unsigned_char(const struct sail_variant *variant);
+
+/*
+ * Returns the variant short value. Behavior is undefined if the variant is not valid
+ * or its type is not short.
+ */
+SAIL_EXPORT short sail_variant_to_short(const struct sail_variant *variant);
+
+/*
+ * Returns the variant unsigned short value. Behavior is undefined if the variant is not valid
+ * or its type is not unsigned short.
+ */
+SAIL_EXPORT unsigned short sail_variant_to_unsigned_short(const struct sail_variant *variant);
+
+/*
+ * Returns the variant integer value. Behavior is undefined if the variant is not valid
+ * or its type is not int.
+ */
+SAIL_EXPORT int sail_variant_to_int(const struct sail_variant *variant);
+
+/*
+ * Returns the variant unsigned int value. Behavior is undefined if the variant is not valid
+ * or its type is not unsigned int.
+ */
+SAIL_EXPORT unsigned int sail_variant_to_unsigned_int(const struct sail_variant *variant);
+
+/*
+ * Returns the variant long value. Behavior is undefined if the variant is not valid
+ * or its type is not long.
+ */
+SAIL_EXPORT long sail_variant_to_long(const struct sail_variant *variant);
+
+/*
+ * Returns the variant unsigned long value. Behavior is undefined if the variant is not valid
+ * or its type is not unsigned long.
+ */
+SAIL_EXPORT unsigned long sail_variant_to_unsigned_long(const struct sail_variant *variant);
+
+/*
+ * Returns the variant string value. Behavior is undefined if the variant is not valid
+ * or its type is not string.
+ */
+SAIL_EXPORT char* sail_variant_to_string(const struct sail_variant *variant);
+
+/*
+ * Returns the variant data value. Behavior is undefined if the variant is not valid
+ * or its type is not data.
+ */
+SAIL_EXPORT void* sail_variant_to_data(const struct sail_variant *variant);
+
+/*
+ * Checks the variant is not NULL and holds a valid value.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_check_variant_valid(const struct sail_variant *variant);
+
+/*
+ * Makes a deep copy of the specified variant.
+ *
+ * Returns SAIL_OK on success.
+ */
+SAIL_EXPORT sail_status_t sail_copy_variant(const struct sail_variant *source, struct sail_variant **target);
+
+/* extern "C" */
+#ifdef __cplusplus
+}
+#endif
+
+#endif
