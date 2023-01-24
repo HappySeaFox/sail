df2ac0f6734d9aa9e54acfc1fa6a243fdb85f437
Dmitry Baryshev <dima8w@gmail.com>
2023-01-24 15:30:39 +0300

    SAIL: Create new cache variables for the bswap test results

--- cmake/sail_check_builtin_bswap.cmake
+++ cmake/sail_check_builtin_bswap.cmake
@@ -11,6 +11,18 @@ function(sail_check_builtin_bswap)
         "
         SAIL_HAVE_BUILTIN_BSWAP16
         )
+        # This variable is used for displaying the test result with message()
+        # in the main CMake file.
+        #
+        # The cache SAIL_HAVE_BUILTIN_BSWAP16 variable that is created by the test
+        # has a value of 1 or an empty string which is not a user friendly value.
+        #
+        if (SAIL_HAVE_BUILTIN_BSWAP16)
+            set(SAIL_HAVE_BUILTIN_BSWAP16_DISPLAY ON CACHE INTERNAL "")
+        else()
+            set(SAIL_HAVE_BUILTIN_BSWAP16_DISPLAY OFF CACHE INTERNAL "")
+        endif()
+
         check_c_source_compiles(
         "
             int main(int argc, char *argv[]) {
@@ -20,6 +32,12 @@ function(sail_check_builtin_bswap)
         "
         SAIL_HAVE_BUILTIN_BSWAP32
         )
+        if (SAIL_HAVE_BUILTIN_BSWAP32)
+            set(SAIL_HAVE_BUILTIN_BSWAP32_DISPLAY ON CACHE INTERNAL "")
+        else()
+            set(SAIL_HAVE_BUILTIN_BSWAP32_DISPLAY OFF CACHE INTERNAL "")
+        endif()
+
         check_c_source_compiles(
         "
             int main(int argc, char *argv[]) {
@@ -29,5 +47,10 @@ function(sail_check_builtin_bswap)
         "
         SAIL_HAVE_BUILTIN_BSWAP64
         )
+        if (SAIL_HAVE_BUILTIN_BSWAP64)
+            set(SAIL_HAVE_BUILTIN_BSWAP64_DISPLAY ON CACHE INTERNAL "")
+        else()
+            set(SAIL_HAVE_BUILTIN_BSWAP64_DISPLAY OFF CACHE INTERNAL "")
+        endif()
     cmake_pop_check_state()
 endfunction()
