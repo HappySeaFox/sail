4b06fe2ed017ac14e88dd5a57bd195dd6a15e64f
Dzmitry Baryshau <dima8w@gmail.com>
2025-10-25 22:14:22 +0300

    SAIL: Remove ASAN messages from CMake

--- cmake/sail_enable_asan.cmake
+++ cmake/sail_enable_asan.cmake
@@ -3,9 +3,9 @@
 macro(sail_enable_asan)
     cmake_parse_arguments(SAIL_ASAN "" "TARGET" "" ${ARGN})
 
-    # Enable ASAN on the specified target in DEV mode only. We try to compile and run
+    # Enable ASAN on the specified target in DEV mode only. We try to compile and RUN
     # a test program with -fsanitize=address as ASAN may be accessible in MSVC 2019 on Windows 7,
-    # but the resulting program fails to run due to missing symbols in kernel32.
+    # but the resulting program may fail to run due to missing symbols in kernel32.
     #
     if (SAIL_DEV)
         if (NOT MSVC OR MSVC_TOOLSET_VERSION GREATER 141)
@@ -29,16 +29,12 @@ macro(sail_enable_asan)
         endif()
 
         if (SAIL_HAVE_ASAN)
-            message("${SAIL_ASAN_TARGET}: ASAN is enabled")
-
             target_compile_options(${SAIL_ASAN_TARGET} PRIVATE "-fsanitize=address")
 
             # GCC and Clang require to link ASAN as well
             if (CMAKE_C_COMPILER_ID STREQUAL "GNU" OR CMAKE_C_COMPILER_ID MATCHES "Clang")
                 target_link_libraries(${SAIL_ASAN_TARGET} PRIVATE "-fsanitize=address")
             endif()
-        else()
-            message("${SAIL_ASAN_TARGET}: ASAN is not supported with this ${CMAKE_C_COMPILER_ID} compiler.")
         endif()
     endif()
 endmacro()
