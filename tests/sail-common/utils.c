245abf13ef14d31456e2d4ab3aea1ecb6ccc30f9
Dzmitry Baryshau <dima8w@gmail.com>
2026-05-23 10:36:51 +0300

    TESTS: Added more tests for sail_pixels_buffer_size and sail_size_mul

--- tests/sail-common/utils.c
+++ tests/sail-common/utils.c
@@ -79,19 +79,35 @@ static MunitResult test_size_mul(const MunitParameter params[], void* user_data)
     munit_assert_int(sail_size_mul(100u, 200u, &result), ==, SAIL_OK);
     munit_assert_size(result, ==, 20000u);
 
+    munit_assert_int(sail_size_mul(200u, 100u, &result), ==, SAIL_OK);
+    munit_assert_size(result, ==, 20000u);
+
+    munit_assert_int(sail_size_mul(1u, 1u, &result), ==, SAIL_OK);
+    munit_assert_size(result, ==, 1u);
+
     munit_assert_int(sail_size_mul(0u, 100u, &result), ==, SAIL_OK);
     munit_assert_size(result, ==, 0u);
 
-    if (SIZE_MAX / 2 >= (size_t)UINT32_MAX)
-    {
-        /* 64-bit size_t: product of two max unsigned values still fits. */
-        munit_assert_int(sail_size_mul(UINT32_MAX, UINT32_MAX, &result), ==, SAIL_OK);
-    }
-    else
-    {
-        /* 32-bit size_t: 65536 * 65536 exceeds SIZE_MAX. */
-        munit_assert_int(sail_size_mul(65536u, 65536u, &result), ==, SAIL_ERROR_INVALID_IMAGE_DIMENSIONS);
-    }
+    munit_assert_int(sail_size_mul(100u, 0u, &result), ==, SAIL_OK);
+    munit_assert_size(result, ==, 0u);
+
+    munit_assert_int(sail_size_mul(0u, 0u, &result), ==, SAIL_OK);
+    munit_assert_size(result, ==, 0u);
+
+    munit_assert_int(sail_size_mul(100u, 200u, NULL), ==, SAIL_ERROR_NULL_PTR);
+
+#if SIZE_MAX > UINT32_MAX
+    /* 64-bit size_t: product of two max unsigned values still fits. */
+    munit_assert_int(sail_size_mul(UINT32_MAX, UINT32_MAX, &result), ==, SAIL_OK);
+    munit_assert_size(result, ==, (size_t)UINT32_MAX * (size_t)UINT32_MAX);
+#else
+    /* 32-bit size_t: 65536 * 65536 exceeds SIZE_MAX. */
+    munit_assert_int(sail_size_mul(65536u, 65536u, &result), ==, SAIL_ERROR_INVALID_IMAGE_DIMENSIONS);
+    munit_assert_int(sail_size_mul(100000u, 50000u, &result), ==, SAIL_ERROR_INVALID_IMAGE_DIMENSIONS);
+
+    munit_assert_int(sail_size_mul(65536u, 65535u, &result), ==, SAIL_OK);
+    munit_assert_size(result, ==, 4294901760u);
+#endif
 
     return MUNIT_OK;
 }
@@ -106,12 +122,23 @@ static MunitResult test_pixels_buffer_size(const MunitParameter params[], void* 
     munit_assert_int(sail_pixels_buffer_size(100u, 300u, &pixels_size), ==, SAIL_OK);
     munit_assert_size(pixels_size, ==, 30000u);
 
+    munit_assert_int(sail_pixels_buffer_size(0u, 300u, &pixels_size), ==, SAIL_OK);
+    munit_assert_size(pixels_size, ==, 0u);
+
     munit_assert_int(sail_pixels_buffer_size(100u, 0u, &pixels_size), ==, SAIL_ERROR_INVALID_BYTES_PER_LINE);
 
-    if (SIZE_MAX / 2 < (size_t)UINT32_MAX)
-    {
-        munit_assert_int(sail_pixels_buffer_size(65536u, 65536u, &pixels_size), ==, SAIL_ERROR_INVALID_IMAGE_DIMENSIONS);
-    }
+    munit_assert_int(sail_pixels_buffer_size(100u, 300u, NULL), ==, SAIL_ERROR_NULL_PTR);
+
+#if SIZE_MAX > UINT32_MAX
+    munit_assert_int(sail_pixels_buffer_size(UINT32_MAX, UINT32_MAX, &pixels_size), ==, SAIL_OK);
+    munit_assert_size(pixels_size, ==, (size_t)UINT32_MAX * (size_t)UINT32_MAX);
+#else
+    munit_assert_int(sail_pixels_buffer_size(65536u, 65536u, &pixels_size), ==, SAIL_ERROR_INVALID_IMAGE_DIMENSIONS);
+    munit_assert_int(sail_pixels_buffer_size(100000u, 50000u, &pixels_size), ==, SAIL_ERROR_INVALID_IMAGE_DIMENSIONS);
+
+    munit_assert_int(sail_pixels_buffer_size(65536u, 65535u, &pixels_size), ==, SAIL_OK);
+    munit_assert_size(pixels_size, ==, 4294901760u);
+#endif
 
     return MUNIT_OK;
 }
